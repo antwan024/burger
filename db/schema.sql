2305c7aedb4ce022759b2f74a7ca4865764f63dd
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-09-25 22:58:08 -0600

    created files and connection

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE burgers_db;
+
+CREATE TABLE burgers (
+
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
+
+);
+
+
+
+-- id: an auto incrementing int that serves as the primary key.
+
+-- burger_name: a string.
+
+-- devoured: a boolean.
