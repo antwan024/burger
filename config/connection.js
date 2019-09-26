350a08e56d5ddae737565f8c74259a0f6a6572f5
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-09-25 23:46:39 -0600

    worked on ORM

--- config/connection.js
+++ config/connection.js
@@ -6,19 +6,20 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "mysql420",
-  database: "burger"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "mysql420",
+    database: "burger"
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
