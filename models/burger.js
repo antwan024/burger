b34c6c6c683a64dabe51c3069f63cd67afd6c1d8
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-09-28 00:14:39 -0600

    Created controller

--- models/burger.js
+++ models/burger.js
@@ -17,12 +17,8 @@ var burger = {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+  
 };
 
-// Export the database functions for the controller (burgersController.js).
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
